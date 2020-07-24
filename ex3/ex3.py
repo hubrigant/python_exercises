659ab580384973356ab94f9d6df289d1a13ea381
Jon Williams <jonhatesspam+gitrepo@gmail.com>
2020-07-24 12:23:46 -0500

    exercise 3 done

---
+++ ex3/ex3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+"""
+    W3Schools Python Exercises
+    Exercise 3
+    24 July 2020
+    Jon Williams
+"""
+
+import datetime
+
+now = datetime.datetime.now()
+print(now.strftime("%A, %b %d %Y %I:%M:%S %p"))
