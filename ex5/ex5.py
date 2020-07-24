489182f93e154b31193186adb55608b40f3b5a9b
Jon Williams <jonhatesspam+gitrepo@gmail.com>
2020-07-24 12:40:57 -0500

    exercise 5 done

---
+++ ex5/ex5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""
+    W3Schools Python Exercises
+    Exercise 5
+    24 July 2020
+    Jon Williams
+"""
+
+(first_name, last_name) = input("Input your name: ").split(" ")
+
+print("Hello {} {}".format(last_name, first_name))
